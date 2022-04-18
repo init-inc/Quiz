7bed34a8aa4875eb8e78c6ef844f3a567dac22ea
yamataku29 <tmn.sby001@gmail.com>
2022-04-18 11:17:02 +0900

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Taku Yamada on 2022/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
