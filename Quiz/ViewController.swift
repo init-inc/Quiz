431b244fe01f4ed4302181a98d8e56b2c49bdb15
yamataku29 <tmn.sby001@gmail.com>
2022-04-20 10:42:06 +0900

    first commit

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var str = ""
+        let i = 3
+        if (i >= 1) { // 真偽値→Bool
+            str = "iは1以上です"
+        }
+        self.label.text = str
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        self.label.text = "ボタンを押しました"
+    }
+    
 }
 
