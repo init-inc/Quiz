cf5f9638117b20fe33914693021a40f9e0f62fb7
yamataku29 <tmn.sby001@gmail.com>
2022-05-12 13:33:44 +0900

    P.125迄

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,19 +12,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        var str = ""
-        let i = 3
-        if (i >= 1) { // 真偽値→Bool
-            str = "iは1以上です"
-        }
-        self.label.text = str
     }
 
     @IBAction func pressButton(_ sender: Any) {
-        self.label.text = "ボタンを押しました"
     }
-    
 }
 
