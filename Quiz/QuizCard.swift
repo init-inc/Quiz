364d7d834ddb4173283282f49276fd155e6a8bf0
yamataku29 <tmn.sby001@gmail.com>
2022-05-30 12:48:00 +0900

    P.202

--- Quiz/QuizCard.swift
+++ Quiz/QuizCard.swift
@@ -12,10 +12,11 @@ class QuizCard: UIView {
     @IBOutlet weak var quizImageView: UIImageView!
     @IBOutlet weak var quizLabel: UILabel!
     
+    // 列挙型
     enum QuizStyle {
-        case initial
-        case right
-        case wrong
+        case initial // 初期状態
+        case right // 正解
+        case wrong // 不正解
     }
     
     var style: QuizStyle = .initial {
@@ -24,5 +25,22 @@ class QuizCard: UIView {
         }
     }
     
-    func setQuizStyle(style: QuizStyle) {}
+    func setQuizStyle(style: QuizStyle) {
+        switch style {
+        case .initial:
+            // 背景色に淡いグレー、アイコンは表示しない
+            self.backgroundColor = UIColor(named: "normal background")
+            self.iconImageView.isHidden = true
+        case .right:
+            // 背景色に緑、アイコンをtrueの画像で表示
+            self.backgroundColor = UIColor(named: "right background")
+            self.iconImageView.isHidden = false
+            self.iconImageView.image = UIImage(named: "right icon")
+        case .wrong:
+            // 背景色に赤、アイコンをfalseの画像で表示
+            self.backgroundColor = UIColor(named: "wrong background")
+            self.iconImageView.isHidden = false
+            self.iconImageView.image = UIImage(named: "wrong icon")
+        }
+    }
 }
--- Quiz/QuizViewController.swift
+++ Quiz/QuizViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class QuizViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var quizCard: QuizCard!
     
     var nameText: String = ""
     
@@ -17,5 +18,50 @@ class QuizViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.label.text = self.nameText
+        
+        self.quizCard.style = .initial
+        // UIPanGestureRecognizerはUI部品をタップした時の処理を記述するためのクラス
+        let panGestureRecognizer = UIPanGestureRecognizer(
+            target: self, action: #selector(dragQuizCard(_:))
+        )
+        self.quizCard.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    @objc func dragQuizCard(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began, .changed:
+            self.transformQuizCard(gesture: sender)
+        case .ended:
+            break
+        default:
+            break
+        }
+    }
+    
+    func transformQuizCard(gesture: UIPanGestureRecognizer) {
+        // スワイプされた時の動きに併せてquizCardを移動させる処理
+        // スワイプ時に移動した距離を取得
+        let translation = gesture.translation(in: self.quizCard)
+        // 移動した距離を元にCGAffineTransformクラスのオブジェクトを取得
+        let translationTransform = CGAffineTransform(
+            translationX: translation.x, y: translation.y
+        )
+        // 画面幅の半分に対する移動した距離の割合を計算
+        let translationPercent = translation.x / UIScreen.main.bounds.width / 2
+        // 割合に対して角度を算出
+        let rotaionAngle = CGFloat.pi / 3 * translationPercent
+        // 算出した角度でのCGAffineTransformクラスオブジェクトを作成
+        let rotationTransform = CGAffineTransform(rotationAngle: rotaionAngle)
+        // 変換したオブジェクトを合成
+        let transform = translationTransform.concatenating(rotationTransform)
+        // 変換したオブジェクトをクイズカードに適用
+        self.quizCard.transform = transform
+        
+        // スワイプされた位置(X軸の座標)に応じてクイズカードの状態を判別
+        if translation.x > 0 {
+            self.quizCard.style = .right
+        } else {
+            self.quizCard.style = .wrong
+        }
     }
 }
